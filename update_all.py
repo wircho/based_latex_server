6a5358dc6f2970731fc3f348e303c04e541b4a7f
Adolfo Rodriguez <adolfo@mokriya.com>
2020-02-18 15:29:50 -0500

    typo

--- update_all.py
+++ update_all.py
@@ -9,7 +9,7 @@ subprocess.run(["curl", "https://api.interoper.io/quit?token=" + token])
 os.remove("action_tokens.json")
 subprocess.run(["git", "pull", "origin", "master"])
 should_clear_all = None
-while should_clear_all != "y" and should_clear_all != "no": should_clear_all = input("Clear all (y/n)? ")
+while should_clear_all != "y" and should_clear_all != "n": should_clear_all = input("Clear all (y/n)? ")
 if should_clear_all == "y": subprocess.run(["python", "clear_all.py"])
 for _ in range(2): subprocess.run(["pip", "install", "based_latex", "--upgrade"])
 print("\nReady!\n\nNow run this:\nsudo nohup /home/ubuntu/anaconda3/envs/latex/bin/python based_latex_server.py 2>&1 </dev/null &\n")
