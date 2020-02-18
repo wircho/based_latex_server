04a01e400e80694755beb44426f3da318baeed80
Adolfo Rodriguez <adolfo@mokriya.com>
2020-02-18 10:35:10 -0500

    update done

--- update_all.py
+++ update_all.py
@@ -6,5 +6,7 @@ from utils import random_string
 token = random_string();
 with open("action_tokens.json", "w") as file: file.write('{"quit": "' + token + '"}')
 subprocess.run(["curl", "https://api.interoper.io/quit?token=" + token])
-subprocess.run(["git", "pull", "origin", "master"])
 os.remove("action_tokens.json")
+subprocess.run(["git", "pull", "origin", "master"])
+for _ in range(2): subprocess.run(["pip", "install", "based_latex", "--upgrade"])
+subprocess.run(["nohup", "/home/ubuntu/anaconda3/envs/latex/bin/python", "based_latex_server.py", "2>&1", "</dev/null", "&"])
