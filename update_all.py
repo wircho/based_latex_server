181da3f213255f877457135b28bbe44e265e939a
Adolfo Rodriguez <adolfo@mokriya.com>
2020-02-18 10:32:21 -0500

    update pulls

--- update_all.py
+++ update_all.py
@@ -6,4 +6,5 @@ from utils import random_string
 token = random_string();
 with open("action_tokens.json", "w") as file: file.write('{"quit": "' + token + '"}')
 subprocess.run(["curl", "https://api.interoper.io/quit?token=" + token])
+subprocess.run(["git", "pull", "origin", "master"])
 os.remove("action_tokens.json")
