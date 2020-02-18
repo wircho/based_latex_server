7e4cddeb7dfc33faf7408652748bf5f30d3bdb00
Adolfo Rodriguez <adolfo@mokriya.com>
2020-02-18 10:29:34 -0500

    interoper

--- update_all.py
+++ update_all.py
@@ -5,5 +5,5 @@ from utils import random_string
 
 token = random_string();
 with open("action_tokens.json", "w") as file: file.write('{"quit": "' + token + '"}')
-subprocess.run(["curl", "https://0.0.0.0:443/quit?token=" + token])
+subprocess.run(["curl", "https://api.interoper.io/quit?token=" + token])
 os.remove("action_tokens.json")
